52e9753b73b59af174c249bddb462be95a62ea16
bhrugen <bhrugen1990@gmail.com>
2025-07-03 15:07:04 -0500

    Section 2 -  Assignment 1 - Components

--- taskopedia/src/main.jsx
+++ taskopedia/src/main.jsx
@@ -5,8 +5,17 @@ const root = createRoot(document.getElementById("root"));
 function HomePage() {
   return (
     <div>
+      <Header />
       <p>We are super excited to learn React</p>
       <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div>
+      <h1>Welcome to the React Course!</h1>
     </div>
   );
 }
