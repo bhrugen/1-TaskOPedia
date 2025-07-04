124f358df51c2c88b127dedae32e42c02ee718bd
bhrugen <bhrugen1990@gmail.com>
2025-07-04 10:10:54 -0500

    Section 3 - Display image for student

--- taskopedia/src/main.jsx
+++ taskopedia/src/main.jsx
@@ -13,8 +13,13 @@ function Students() {
 
   return (
     <div className="container p-4 bg-success my-3 rounded">
-      <div className="row border">
-        <div className="col-2">IMAGE</div>
+      <div className="row border p-2">
+        <div className="col-2">
+          <img
+            src={`https://ui-avatars.com/api/?name=${fullName}`}
+            style={{ maxWidth: "50px" }}
+          ></img>
+        </div>
         <div className="col-8">
           {fullName}
           <br />
